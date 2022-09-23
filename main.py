a4d72c2dc93b9fec324665c7109b527c4ceb1371
Yanush Viktar <vicjan89@gmail.com>
2022-09-23 17:40:50 +0300

    Добавлена фильтрация по расстоянию от начала строки. Добавлен Многолетний график.csv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import config
 
-protections = ("ЛЗШ", "ЗМН", "Контроль УРОВ",
+protections = ("ЛЗШ", "ЗМН", "Контроль УРОВ", "УРОВ",
 			   "АПВ", "АВР", "Блокировка АВР", "Защита от замыканий", "ПРД ", "ПРМ ", "Токовая отсечка",
 			   "ТО ", "МТЗ ", "сквозных КЗ", " ТЗНП", "|ТЗНП", "Делительная", "Дистанц", "НТЗНП", "Защита от обрыва")
 protections_steps = ("1ст", "2ст", "3ст", "4ст", "1 ст", "2 ст", "3 ст", "4 ст", "5 ст", "6 ст")
@@ -20,7 +20,7 @@ def parse_header(input_string: str, header_designation: str) -> list:
 		start = input_string.find('\n', index_end + 1) + 1
 	return headers
 
-def find_prot(input_string: str, prot_name: str) -> list:
+def find_prot(input_string: str, prot_name: str, filter: int = 0) -> list:
 	run = True
 	start = 0
 	list_protections = []
@@ -30,12 +30,15 @@ def find_prot(input_string: str, prot_name: str) -> list:
 		if index_start == -1:
 			run = False
 		else:
+			from_begin_row = input_string.rfind('\n', start, index_start)
 			index_end = index_start + len_prot_name
-			list_protections.append((index_start, index_end))
+			if filter == 0 or (index_start - from_begin_row) < filter:
+				list_protections.append((index_start, index_end))
 			start = index_end
 	return list_protections
 
 count_prot = 0
+count_steps = 0
 
 for group_name in os.walk(config.path_source, topdown=True, onerror=None, followlinks=False):
 	for name in group_name[2]:
@@ -52,26 +55,22 @@ for group_name in os.walk(config.path_source, topdown=True, onerror=None, follow
 					indexes_headers.extend(((i, 3) for i in level2))
 				len_str_ustavki = len(str_ustavki)
 				for prot in protections:
-					fp = find_prot(str_ustavki, prot)
+					fp = find_prot(str_ustavki, prot, filter=32)
 					indexes_headers.extend((i, 4) for i in fp)
 				for step in protections_steps:
-					fp = find_prot(str_ustavki, step)
+					fp = find_prot(str_ustavki, step, filter=32)
 					indexes_headers.extend((i, 5) for i in fp)
 				indexes_headers.sort(key=lambda x: x[0][0])
 				len_indexes_headers = len(indexes_headers)
 				for num in range(len_indexes_headers):
 					item_current = indexes_headers[num]
+					if item_current[1] == 4:
+						count_prot += 1
+					elif item_current[1] == 5:
+						count_steps += 1
 					print(str(item_current[1]) + ' ' * item_current[1] * 3 + str_ustavki[slice(*item_current[0])])
-					# if item_current[1] == 3:
-					# 	if num < (len_indexes_headers - 1):
-					# 		item_next = indexes_headers[num + 1]
-					# 	else:
-					# 		item_next = ((len_str_ustavki, len_str_ustavki), 3)
-					# 	for prot in protections:
-					# 		if prot in str_ustavki[item_current[0][1]:item_next[0][0]]:
-					# 			print('4' + ' ' * 12 + prot)
-					# 			count_prot +=1
-print(f'Всего РЗА: {count_prot}')
+
+print(f'Всего РЗА: {count_prot} а ступеней: {count_steps}')
 
 
 
